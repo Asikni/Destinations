253c9b43e036f9c30ffd0798bb528fc1f9a794e7
Imran <mohdimranshountoo@gmail.com>
2024-03-19 14:33:22 +0530

    code updated

--- src/components/storiesSection.jsx
+++ src/components/storiesSection.jsx
@@ -3,45 +3,51 @@ import Card from "../card";
 import stories from "../cardData";
 
 function StoriesSection() {
-  const[currenStoryIndex, setCurrentStoryIndex] = useState(0)
-const newStory = () => {
-  setCurrentStoryIndex(currenStoryIndex < stories.length -1   ? currenStoryIndex + 1 : 0 )
-}
-const oldStory = () => {
-  setCurrentStoryIndex(currenStoryIndex > 0 ? currenStoryIndex - 1 : stories.length -1 )
-}
+  const [currenStoryIndex, setCurrentStoryIndex] = useState(0);
+  const newStory = () => {
+    setCurrentStoryIndex(
+      currenStoryIndex < stories.length - 1 ? currenStoryIndex + 1 : 0
+    );
+  };
+  const oldStory = () => {
+    setCurrentStoryIndex(
+      currenStoryIndex > 0 ? currenStoryIndex - 1 : stories.length - 1
+    );
+  };
 
   return (
     <div
       className="Stories"
-      style={{ backgroundColor: "#1E2126", height: "100vh", }}
+      style={{ backgroundColor: "#1E2126", height: "100vh" }}
     >
       <div className="travelStoriesHeading">Travel Stories</div>
       <div
         className="heroTravelSection"
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
       >
-        <div className="leftArrowTravelSection"  onClick={oldStory}>
+        <div className="leftArrowTravelSection" onClick={oldStory}>
           &lt;
         </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-        
+        <div
+          className="cardTemp"
+          style={{
+            display: "flex",
+            justifyContent: "center",
             
-            <Card
-
-              story={stories[currenStoryIndex].comment}
-              name={stories[currenStoryIndex].from}
-              works={stories[currenStoryIndex].worksAt}
-            />
-      
+          }}
+        >
+          <Card
+            story={stories[currenStoryIndex].comment}
+            name={stories[currenStoryIndex].from}
+            works={stories[currenStoryIndex].worksAt}
+          />
         </div>
 
-        <div
-          className="rightArrowTravelSection"
-          onClick={newStory}
-          
-
-        >
+        <div className="rightArrowTravelSection" onClick={newStory}>
           &gt;
         </div>
       </div>
